8670ef5601e223ff53217af9f2c5c9c9cfacd7e0
WeissRobin <105072073+WeissRobin@users.noreply.github.com>
2024-08-27 23:11:25 +0200

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,6 +12,8 @@ const Projects = () => {
             <ProjectItem name='CV Generator' purpose='Odin Project' url='https://profilifyq.netlify.app' technologies='React / SCSS' />
             <ProjectItem name='Memory Game' purpose='Odin Project' url='https://memo-game-pokemon.netlify.app' technologies='React / Tailwind / API' />
             <ProjectItem name='Restaurant' purpose='Odin Project' url='https://sakuratei.netlify.app/' technologies='Only Desktop / Webpack / SCSS' />
+            <ProjectItem name='Signup & Login System' purpose='PHP Self-learn' url='https://github.com/WeissRobin/sign-up-login-system' technologies='PHP / SCSS / CHART-JS' />
+            <ProjectItem name='Future Studio' purpose='Hiring Project' url='https://future-studio-hiring-project.netlify.app' technologies='HTML / JS / Bootstrap' />
             <ProjectItem name='More Here' purpose='GitHub' url='https://github.com/WeissRobin?tab=repositories' technologies='' />
         </div>
     );
